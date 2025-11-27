58943797e96c9bc94cbe3ff3d7b1c7a0221b3016
Rennan Andres Paloschi <rennan_paloschi@yahoo.com>
2025-11-27 02:08:04 +0000

    modificando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Djeice, você é a rosa 🌹 que eu plantei e virou uma linda roseira, me dando outras rosinhas 🌹🌹🌹</h1>
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <p style={{ fontSize: "1.5rem", lineHeight: "1.6" }}>
+        Djeice, você é a rosa 🌹 que eu plantei e que virou uma linda roseira,
+        me presenteando com outras rosinhas 🌹🌹🌹
+      </p>
+    </div>
+  );
 }
 
 export default Home;
